10b79ac5906fdce1f40345c24fd6668fa86f59c2
Your Name <you@example.com>
2023-04-02 01:46:11 +0000

    Basketball table code.

--- app.py
+++ app.py
@@ -12,6 +12,23 @@ def testing():
     conn.close()
     return "Database Connection Successful."
 
+@app.route('/db_create')
+def creating():
+    conn=psycopg2.connect("postgres://lab_10_cspb_3308_database_user:QyGIpeJXm5FAO3CqkdvykoTXfNpLFRL2@dpg-cgkaoqe4dad69r1ngi60-a/lab_10_cspb_3308_database")
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+        );
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created."
+
 
 # from flask import Flask
 # app = Flask(__name__)
