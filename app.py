e9797f50a6b4b7ffde74f7a7e4022cd344a2c881
Your Name <you@example.com>
2023-04-02 01:38:06 +0000

    Push edits.

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def hello_world():
 def testing():
     conn=psycopg2.connect("postgres://lab_10_cspb_3308_database_user:QyGIpeJXm5FAO3CqkdvykoTXfNpLFRL2@dpg-cgkaoqe4dad69r1ngi60-a/lab_10_cspb_3308_database")
     conn.close()
-    return "Database Connection Successful"
+    return "Database Connection Successful."
 
 
 # from flask import Flask
