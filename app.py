7357cb11ae6567946babddef6c4cefcf0f6df706
Micah614 <59854631+Micah614@users.noreply.github.com>
2023-04-01 16:32:28 -0800

    Update app.py
    Change file being read by render. (1st Deploy)

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/db_test')
+def testing():
+    conn=psycopg2.connect("postgres://lab_10_cspb_3308_database_user:QyGIpeJXm5FAO3CqkdvykoTXfNpLFRL2@dpg-cgkaoqe4dad69r1ngi60-a/lab_10_cspb_3308_database")
+    conn.close()
+    return "Database Connection Successful"
+
+
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
