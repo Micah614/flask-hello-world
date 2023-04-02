b35c4578a40d7e02a3f1eada9f8628b3d455bf1d
Your Name <you@example.com>
2023-04-02 04:56:30 +0000

    MUCH simpler.

--- app.py
+++ app.py
@@ -79,47 +79,9 @@ def selecting():
     conn=psycopg2.connect("postgres://lab_10_cspb_3308_database_user:QyGIpeJXm5FAO3CqkdvykoTXfNpLFRL2@dpg-cgkaoqe4dad69r1ngi60-a/lab_10_cspb_3308_database")
     cursor = conn.cursor()
     cursor.execute('''SELECT * FROM Basketball;''')
-    data = cursor.fetchall()
-    render_template('table.html', data=data)
+    records = cursor.fetchall()
+    # return render_template('table.html', data=records)
+    return render_template('table.html', data=records)
     conn.commit()
     conn.close()
-    return "Success!"
-
-    # for row in records: 
-    #     first = row[0]
-    #     last = row[1]
-    #     City = row[2]
-    #     Name = row[3]
-    #     Number = row[4]
     
-
-
-
-
-# # Declare your table
-# class ItemTable(Table):
-#     name = Col('Name')
-#     description = Col('Description')
-
-# # Get some objects
-# class Item(object):
-#     def __init__(self, name, description):
-#         self.name = name
-#         self.description = description
-# items = [Item('Name1', 'Description1'),
-#          Item('Name2', 'Description2'),
-#          Item('Name3', 'Description3')]
-# # Or, equivalently, some dicts
-# items = [dict(name='Name1', description='Description1'),
-#          dict(name='Name2', description='Description2'),
-#          dict(name='Name3', description='Description3')]
-
-# # Or, more likely, load items from your database with something like
-# items = ItemModel.query.all()
-
-# # Populate the table
-# table = ItemTable(items)
-
-# # Print the html
-# print(table.__html__())
-# # or just {{ table }} from within a Jinja template
